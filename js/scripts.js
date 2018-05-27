4edcb83722999594302749a93a85e5e0325b1079
unknown <swm1000@hotmail.com>
2018-05-26 22:36:03 -0400

    dialog box for new category

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,17 @@ $(document).ready(function(){
     $("#tasks").tabs();
     $("ul").sortable({ axis: "x", containment: "#tasks" });
     $("ol").sortable({ axis: "y", containment: "#tasks" });
+    
+    $("#addcategory").button()
+    .click(function() {
+        $("#newcategory").dialog({
+            width: 300,
+            resizeable: false,
+            modal: true,
+            buttons: {
+                "Add new category": function(){},
+                "Cancel": function(){}
+            }
+        });
+    });
 });
