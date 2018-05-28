841472849fe120ee1a4bc10967e880e03c36ef38
unknown <swm1000@hotmail.com>
2018-05-27 21:38:07 -0400

    set new category to active tab

--- js/scripts.js
+++ js/scripts.js
@@ -12,10 +12,13 @@ $(document).ready(function(){
             buttons: {
                 "Add new category": function(){
                     var categoryname = $("#category").val();
-                    $("<li><a href='#'" + categoryname + ">" + categoryname + "</a></li>").appendTo("#categories");
+                    $("<li><a href='#'" + categoryname + ">" + categoryname + "</a></li>").appendTo("#categories"); //new category
+                    $("<ol id='" + categoryname + "'></ol>").appendTo("#tasks"); //items for category
+                    $("#tasks").tabs("refresh"); //for tab formatting
+                    var numtabs = $("#tasks .ui-tabs-nav li").length;
+                    $("#tasks").tabs("option", "active", numtabs-1);
                     $("#category").val("");
                     $(this).dialog("close");
-                    $("#tasks").tabs("refresh");
                 },
                 "Cancel": function(){
                     $("#category").val("");
