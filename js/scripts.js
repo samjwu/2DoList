fe83782702519b4d1fbaec44ff2c5ae06262db28
unknown <swm1000@hotmail.com>
2018-05-28 20:45:45 -0400

    implement add task button/function

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    $("#tasks").tabs();
+$(document).ready(function() {
+    $("#alltasks").tabs();
     $("ul").sortable({ axis: "x", containment: "#tasks" });
     $("ol").sortable({ axis: "y", containment: "#tasks" });
     
@@ -10,18 +10,41 @@ $(document).ready(function(){
             resizeable: false,
             modal: true,
             buttons: {
-                "Add new category": function(){
+                "Add": function() {
                     var categoryname = $("#category").val();
                     $("<li><a href='#'" + categoryname + ">" + categoryname + "</a></li>").appendTo("#categories"); //new category
-                    $("<ol id='" + categoryname + "'></ol>").appendTo("#tasks"); //items for category
-                    $("#tasks").tabs("refresh"); //for tab formatting
-                    var numtabs = $("#tasks .ui-tabs-nav li").length;
-                    $("#tasks").tabs("option", "active", numtabs-1);
+                    $("<ol id='" + categoryname + "'></ol>").appendTo("#alltasks"); //items for category
+                    $("#alltasks").tabs("refresh"); //for tab formatting
+                    var numtabs = $("#alltasks .ui-tabs-nav li").length;
+                    $("#alltasks").tabs("option", "active", numtabs-1);
                     $("#category").val("");
                     $(this).dialog("close");
                 },
-                "Cancel": function(){
+                "Cancel": function() {
                     $("#category").val("");
+                    $(this).dialog("close");
+                }
+            }
+        });
+    });
+
+    $("#addtask").button()
+    .click(function() {
+        $("#newtask").dialog({
+            width: 300,
+            resizeable: false,
+            modal: true,
+            buttons: {
+                "Add": function() {
+                    var taskname = $("#task").val();
+                    var activetabindex = $("#alltasks").tabs("option", "active");
+                    var activetab = $("#alltasks ul li a").eq(activetabindex).attr("href");
+                    $("<li><input type='checkbox'>" + taskname + "</li>").appendTo(activetab);
+                    $("#task").val("");
+                    $(this).dialog("close");
+                },
+                "Cancel": function() {
+                    $("#task").val("");
                     $(this).dialog("close");
                 }
             }
